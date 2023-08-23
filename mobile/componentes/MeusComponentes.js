bdcca72e4d6953010e2bb5452fddc25e53095025
Yproshot <maiconpacheco@live.com>
2023-08-22 22:12:51 -0300

    commit 01

---
+++ mobile/componentes/MeusComponentes.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native/types";
+
+export default function MeuComponente() {
+    return(
+            <View>
+                <Text>
+                    MeuComponente
+                </Text>
+            </View>
+    )
+}
