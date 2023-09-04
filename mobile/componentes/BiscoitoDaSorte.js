953450d97895197db8674974b23343953decf04d
Yproshot <maiconpacheco@live.com>
2023-09-04 17:25:08 -0300

    IMG

---
+++ mobile/componentes/BiscoitoDaSorte.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function BiscoitoDaSorte() {
+
+    
+
+  return (
+    <View>
+      <Text>BiscoitoDaSorte</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
