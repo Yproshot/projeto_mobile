55a3824c9f6d09612ad5596e3d40e844bdaa9858
Yproshot <maiconpacheco@live.com>
2023-09-04 14:53:15 -0300

    att 0.1

--- mobile/componentes/Contador.js
+++ mobile/componentes/Contador.js
@@ -1,12 +1,49 @@
-import { requireNativeComponent, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default function Contador() {
-  return (
-    <View>
-      <Text>Contador</Text>
-    </View>
-  )
+
+    const [valor, setValor] = useState(0)
+
+    // let valor = 0
+
+    function incrementar() {
+        setValor(valor + 1)
+    }
+
+    function decrementar() {
+        setValor(valor - 1)
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>VOLUME</Text>
+            <Text style={styles.text}>{valor}</Text>
+            <View style={styles.botao}>
+                <Button
+                    title='+ INCREMENTAR'
+                    onPress={incrementar}
+                />
+                <Button
+                    title='- DECREMENTAR'
+                    onPress={decrementar}
+                />
+            </View>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 30,
+    },
+    botao: {
+        margin: 10
+    }
+})
